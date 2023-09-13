451e72a4b09a9da8f9a52ca4aa0d0f2692380279
codeawy <codeawy@gmail.com>
2023-09-13 16:11:45 +0300

    Cart length

--- src/app/features/cart/cartSlice.ts
+++ src/app/features/cart/cartSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IProduct } from "../../../interface";
 import { addItemToShoppingCart } from "../../../utils/functions";
+import { RootState } from "../../store";
 
 interface CounterState {
   cartItems: IProduct[];
@@ -21,5 +22,6 @@ const cartSlice = createSlice({
 });
 
 export const { addItemToCartAction } = cartSlice.actions;
+export const cartSelector = ({ cart }: RootState) => cart;
 
 export default cartSlice.reducer;
